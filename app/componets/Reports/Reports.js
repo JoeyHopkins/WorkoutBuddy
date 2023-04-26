107aac3377284565ed4875c446d6ae98d6216ac4
Joey <joseph.hopkins1095@gmail.com>
2023-04-25 20:15:18 -0600

    Added right hand android drawer to Reports.js

--- app/componets/Reports/Reports.js
+++ app/componets/Reports/Reports.js
@@ -1,22 +1,73 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useRef, useState} from 'react';
+import { Button, DrawerLayoutAndroid, Text, StyleSheet, View, } from 'react-native';
 
-//https://reactnavigation.org/docs/native-stack-navigator/#options
 export const Reports = ({navigation}) => {
-  return (
-    <Button
-      title="Go to Workout Page"
-      onPress={() =>
-        navigation.navigate('Workout', {name: 'Jane'})
-      }
-    />
+  const drawer = useRef(null);
+  const [drawerPosition, setDrawerPosition] = useState('right');
+
+  const navigationView = () => (
+    <View style={[styles.container, styles.navigationContainer]}>
+      <Button
+        title="Close drawer"
+        onPress={() => drawer.current.closeDrawer()}
+      />
+    </View>
   );
-}
+
+  return (
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition={drawerPosition}
+      renderNavigationView={navigationView}>
+      <View style={styles.container}>
+        <Button
+          title="Open drawer"
+          onPress={() => drawer.current.openDrawer()}
+        />
+      </View>
+    </DrawerLayoutAndroid>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
+  },
+  navigationContainer: {
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    padding: 16,
+    fontSize: 15,
+    textAlign: 'center',
   },
 });
+
+
+
+
+
+//https://reactnavigation.org/docs/native-stack-navigator/#options
+// export const Reports = ({navigation}) => {
+//   return (
+//     <Button
+//       title="Go to Workout Page"
+//       onPress={() =>
+//         navigation.navigate('Workout', {name: 'Jane'})
+//       }
+//     />
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
