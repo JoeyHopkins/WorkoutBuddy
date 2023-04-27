4227292e00c2b6794f0441672fdfd05568dda9e2
Joey <joseph.hopkins1095@gmail.com>
2023-04-26 21:57:13 -0600

    Added expo SQLite and setup to create new table when app is started if doesn't exist. Added code to read tables in the db

--- App.js
+++ App.js
@@ -4,11 +4,49 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Reports } from './app/componets/Reports/Reports';
 import { Workout } from './app/componets/Workout/Workout';
 import { Planning } from './app/componets/Planning/Planning';
+import { useState, useEffect } from 'react'
+import * as SQLite from 'expo-sqlite'
 
 const Tab = createBottomTabNavigator();
+const db = SQLite.openDatabase('workouBuddy.db');
+
+fetchData = () => {
+  db.transaction(tx => {
+    // sending 4 arguments in executeSql
+    tx.executeSql("SELECT name FROM sqlite_master WHERE type='table'", null, 
+      // success callback which sends two things Transaction object and ResultSet Object
+      (txObj, { rows: { _array } }) => console.log(_array),
+      // failure callback which sends two things Transaction object and Error
+      (txObj, error) => console.log('Error ', error)
+      ) // end executeSQL
+  }) // end transaction
+}
 
 //https://reactnavigation.org/docs/native-stack-navigator/#options
 export default function App() {
+
+  const createTables = () => {
+    db.transaction(txn => {
+      txn.executeSql(
+        `CREATE TABLE IF NOT EXISTS reports (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(20))`,
+        [],
+        (sqlTxn, res) => {
+          console.log("Table created successfully")
+          // print off tables in db
+          // this.fetchData() 
+        },
+        error => {
+          console.log("error creating table " + error.message)
+        }
+      )
+    });
+  };
+
+  useEffect( () => {
+    createTables();
+  }, [])
+
+  console.log('app started')
   return (
     <NavigationContainer>
       <Tab.Navigator>
