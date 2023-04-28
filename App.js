430d74aa7d1782cb1efd2914aac25bd781a5d9d0
Joey <joseph.hopkins1095@gmail.com>
2023-04-28 14:01:38 -0600

    added new table, got working on android devices and separated into its own controller file

--- App.js
+++ App.js
@@ -1,57 +1,28 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Reports } from './app/componets/Reports/Reports';
 import { Workout } from './app/componets/Workout/Workout';
 import { Planning } from './app/componets/Planning/Planning';
-import { useState, useEffect } from 'react'
-import * as SQLite from 'expo-sqlite'
+import appSql from './app/controllers/app.controller';
+import { useEffect } from 'react'
 
 const Tab = createBottomTabNavigator();
-const db = SQLite.openDatabase('workouBuddy.db');
-
-fetchData = () => {
-  db.transaction(tx => {
-    // sending 4 arguments in executeSql
-    tx.executeSql("SELECT name FROM sqlite_master WHERE type='table'", null, 
-      // success callback which sends two things Transaction object and ResultSet Object
-      (txObj, { rows: { _array } }) => console.log(_array),
-      // failure callback which sends two things Transaction object and Error
-      (txObj, error) => console.log('Error ', error)
-      ) // end executeSQL
-  }) // end transaction
-}
 
 //https://reactnavigation.org/docs/native-stack-navigator/#options
 export default function App() {
 
-  const createTables = () => {
-    db.transaction(txn => {
-      txn.executeSql(
-        `CREATE TABLE IF NOT EXISTS reports (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(20))`,
-        [],
-        (sqlTxn, res) => {
-          console.log("Table created successfully")
-          // print off tables in db
-          // this.fetchData() 
-        },
-        error => {
-          console.log("error creating table " + error.message)
-        }
-      )
-    });
-  };
-
   useEffect( () => {
-    createTables();
-  }, [])
+    appSql.createTables();
+    appSql.getTablesFromDB();
+  })
 
   console.log('app started')
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Reports" component={Reports} />
         <Tab.Screen name="Workout" component={Workout} />
+        <Tab.Screen name="Reports" component={Reports} />
         <Tab.Screen name="Planning" component={Planning} />
       </Tab.Navigator>
     </NavigationContainer>
---
+++ app/controllers/app.controller.js
@@ -0,0 +1,37 @@
+import * as SQLite from 'expo-sqlite'
+const db = SQLite.openDatabase('workouBuddy.db');
+
+exports.getTablesFromDB = () => {
+  db.transaction(tx => {
+    // sending 4 arguments in executeSql
+    tx.executeSql("SELECT name FROM sqlite_master WHERE type='table'", null, 
+      // success callback which sends two things Transaction object and ResultSet Object
+      (txObj, { rows: { _array } }) => console.log(_array),
+      // failure callback which sends two things Transaction object and Error
+      (txObj, error) => console.log('Error ', error)
+      ) // end executeSQL
+  }) // end transaction
+};
+
+exports.createTables = () => {
+  db.transaction(txn => {
+    txn.executeSql(`CREATE TABLE IF NOT EXISTS reports (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(20));`,
+      [],
+      (sqlTxn, res) => {
+        console.log("reports table created successfully")
+      },
+      error => {
+        console.log("error creating table " + error.message)
+      }
+    )
+    txn.executeSql(`CREATE TABLE IF NOT EXISTS weight (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(20));`,
+      [],
+      (sqlTxn, res) => {
+        console.log("weight table created successfully")
+      },
+      error => {
+        console.log("error creating table " + error.message)
+      }
+    )
+  });
+};
