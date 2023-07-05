75cff548d16a3ec497a3fdbce7e24701296c4405
Joey <joseph.hopkins1095@gmail.com>
2023-07-05 11:30:33 -0600

    Fixed issue where routines were being duplicated when coming back from the edit workout page

--- app/componets/Workout/Workout.js
+++ app/componets/Workout/Workout.js
@@ -78,6 +78,7 @@ export const Workout = ({navigation}) => {
 
   async function getData() {
     try {
+      strengthRoutines.current = []
       let routinesList = await homeSql.getAllRoutinesList()
       seperateRoutinesByType(routinesList)
 
