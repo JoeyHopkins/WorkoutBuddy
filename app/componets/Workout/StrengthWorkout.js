54ac4416a0d65dc409d8c66f3f02167f61e04043
Joey <joseph.hopkins1095@gmail.com>
2023-07-09 16:30:09 -0600

    More basic frontend setup

--- app/componets/Workout/StrengthWorkout.js
+++ app/componets/Workout/StrengthWorkout.js
@@ -1,22 +1,68 @@
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet, Pressable } from "react-native"
+import * as Colors from '../../config/colors'
 
 export const StrengthWorkout = ({ navigation, setPageMode, workouts }) => {
 
   const Record = ({ item }) => {
     return (
-      <View style={{ marginBottom: 10 }}>
-        <Text>ID: {item.id}</Text>
-        <Text>Name: {item.name}</Text>
-        <Text>Routine ID: {item.routineId}</Text>
+      <View style={[styles.homeContainer]}>
+        <Text style={styles.title}>{item.name}</Text>
+
+        <View style={[styles.center]}>
+          <Text style={styles.text}>Set 1</Text>
+        </View>
+
       </View>
     );
   };
 
   return (
-    <View>
-      {workouts.map((item, index) => (
-        <Record key={index} item={item} />
-      ))}
-    </View>
-  )
+    <>
+      <View style={styles.fillSpace}>
+        {workouts.map((item, index) => (
+          <Record key={index} item={item} />
+        ))}
+        <Pressable
+          style={styles.button}
+        >
+          <Text style={styles.text}>Add Activity</Text>
+        </Pressable>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  },
+  fillSpace: {
+    flex: 1,
+  },
+  homeContainer: {
+    backgroundColor: Colors.background,
+    borderWidth: 1,
+    borderRadius: 20,
+    marginTop: 10,
+    marginHorizontal: 20,
+    borderColor: Colors.primary,
+  },
+  title: {
+    color: Colors.primary,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 10,
+  },
+  button: {
+    marginVertical: 10,
+    backgroundColor: Colors.primary,
+    paddingVertical: 10,
+    marginHorizontal: 20,
+    borderRadius: 20,
+  },
+})
