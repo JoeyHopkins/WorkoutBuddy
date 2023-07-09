955f8b9388469c2172f555183b8e3003ec39a5a2
Joey <joseph.hopkins1095@gmail.com>
2023-07-09 15:07:47 -0600

    setup base for strength workout page

--- app/componets/Workout/StrengthWorkout.js
+++ app/componets/Workout/StrengthWorkout.js
@@ -1,7 +1,22 @@
-import { Text } from "react-native"
+import { Text, View } from "react-native"
 
 export const StrengthWorkout = ({ navigation, setPageMode, workouts }) => {
+
+  const Record = ({ item }) => {
+    return (
+      <View style={{ marginBottom: 10 }}>
+        <Text>ID: {item.id}</Text>
+        <Text>Name: {item.name}</Text>
+        <Text>Routine ID: {item.routineId}</Text>
+      </View>
+    );
+  };
+
   return (
-    <Text>Strength</Text>
+    <View>
+      {workouts.map((item, index) => (
+        <Record key={index} item={item} />
+      ))}
+    </View>
   )
 }
