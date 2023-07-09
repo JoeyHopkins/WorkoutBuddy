4d80d0b6125dc05babca053ce9619ca43ecb666c
Joey <joseph.hopkins1095@gmail.com>
2023-07-08 18:03:08 -0600

    Setup controller to add new workouts to new cardio l2 table

--- app/controllers/cardioWorkouts.controller.js
+++ app/controllers/cardioWorkouts.controller.js
@@ -19,5 +19,18 @@ exports.insertL1CardioWorkout = (workoutId, duration, intensity, date) => {
 };
 
 exports.insertL2CardioWorkout = (workoutId, duration, intensity, date, distance) => {
-  console.log(workoutId, duration, intensity, date, distance);
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        "INSERT INTO cardioWorkoutsL2 (workoutId, duration, intensity, date, distance) VALUES (?, ?, ?, ?, ?)",
+        [workoutId, duration, intensity, date],
+        () => {
+          resolve();
+        },
+        (txObj, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
 };
