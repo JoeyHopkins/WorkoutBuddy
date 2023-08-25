6e00bcaf4d1d68f31ca0ebaf0c0654a272c570b2
Joey <joseph.hopkins1095@gmail.com>
2023-08-25 00:00:24 -0600

    Setup workout title on history page

--- app/componets/Workout/WorkoutHistory.js
+++ app/componets/Workout/WorkoutHistory.js
@@ -3,14 +3,16 @@ import styles from '../../config/styles';
 import * as historySql from '../../controllers/workoutHistory.controller'
 import React, {useState, useEffect, useRef, memo} from 'react';
 import { showMessage } from "react-native-flash-message";
+import * as homeSql from '../../controllers/home.controller'
 
 const width = Dimensions.get('window').width;
 const slideWidth = width * 0.8 - 0;
 
 export const WorkoutHistory = ({navigation, route}) => {
-  const { workoutID } = route.params;
+  const { workoutID, workoutName } = route.params;
   const [loading, setLoading] = useState(true);
   const [history, setHistory] = useState([])
+  const routineList = useRef([])
 
   useEffect(() => {
     fetchData()
@@ -49,6 +51,9 @@ export const WorkoutHistory = ({navigation, route}) => {
   return (
     <>
       <ScrollView>
+        <View style={[styles.center]}>
+          <Text style={[styles.title, styles.marginVertical_S]}>{workoutName}</Text>
+        </View>
         <View style={styles.fillSpace}>
           {history.map((workout, index) => (
             <HistoryItem key={index} workout={workout} index={index} />
